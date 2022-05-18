d029375a63fbeba82c755ceb08e1779e2c9dd967
inzapp <inzapp@naver.com>
2022-05-18 15:17:07 +0900

    [nspp.py] split evaluate to each train, validation function, forward origin data with plot predicted data

--- nspp.py
+++ nspp.py
@@ -51,14 +51,22 @@ class NasdaqStockPricePredictor:
         #     for line in f.readlines():
         #         val = float(line.split(',')[1])
         #         data.append(val)
-        self.plot(data)
         split = int(len(data) * (1.0 - self.validation_ratio))
         train_data = data[:split]
         validation_data = data[split:]
+        self.plot_train_validation_data(train_data, validation_data)
         assert len(train_data) > self.time_step and len(validation_data) > self.time_step
         train_x, train_y = self.convert_time_series_data(train_data, self.time_step)
         validation_x, validation_y = self.convert_time_series_data(validation_data, self.time_step)
         return train_data, validation_data, train_x, train_y, validation_x, validation_y
+
+    def plot_train_validation_data(self, train_data, validation_data):
+        len_train = len(train_data)
+        len_validation = len(validation_data)
+        len_sum = len_train + len_validation
+        padded_train_data = train_data.tolist() + [None for _ in range(len_sum - len_train)]
+        padded_validation_data = [None for _ in range(len_sum - len_validation - 1)] + [train_data[-1]] + validation_data.tolist()
+        self.plot([padded_train_data, padded_validation_data], [f'{self.ticker} train', f'{self.ticker} validation'])
 
     def convert_time_series_data(self, data, time_step):
         batch_x = []
@@ -85,12 +93,9 @@ class NasdaqStockPricePredictor:
         arr *= max_val
         return arr
 
-    def plot(self, y_true, y_pred=None):
-        plt.plot(y_true)
-        legend = [self.ticker]
-        if y_pred is not None:
-            plt.plot(y_pred)
-            legend.append('AI predicted')
+    def plot(self, datas, legend):
+        for data in datas:
+            plt.plot(data)
         plt.legend(legend)
         plt.tight_layout(pad=0.5)
         plt.show()
@@ -142,52 +147,63 @@ class NasdaqStockPricePredictor:
                 batch_index += 1
                 print(f'\r[{iteration_count:6d} iter] loss => {loss:.4f}', end='')
                 if iteration_count % 5000 == 0:
-                    loss = self.evaluate(self.model, mode='validation')
-                    self.model.save(f'checkpoints/{self.ticker}_{iteration_count}_iter_{loss:.2f}_val_loss.h5', include_optimizer=False)
+                    loss = self.evaluate_validation()
+                    self.model.save(f'checkpoints/{self.ticker}_{iteration_count}_iter_{loss:.2f}_mae.h5', include_optimizer=False)
                 if iteration_count == self.max_iteration_count:
                     break_flag = True
                     break
             if break_flag:
                 break
+        self.evaluate_train(show_plot=True)
+        self.evaluate_validation(show_plot=True, future_step=self.future_step)
 
-        self.evaluate(mode='train', show_plot=True)
-        self.evaluate(mode='train', show_plot=True, future_step=self.future_step)
-        self.evaluate(mode='validation', show_plot=True)
-        self.evaluate(mode='validation', show_plot=True, future_step=self.future_step)
-
-    def evaluate(self, mode='train', show_plot=False, future_step=0):
-        assert mode == 'train' or mode == 'validation'
+    def evaluate_train(self, show_plot=False, future_step=0):
         print()
-        x = None
-        forward_length = 0
-        if mode == 'train':
-            x = list(self.train_data[:self.time_step])
-            y_seq = []
-            forward_length = len(self.train_data) - self.time_step + future_step
-        else:
-            x = list(self.train_data[-self.time_step:])
-            y_seq = []
-            forward_length = len(self.validation_data) + future_step
-        for _ in tqdm(range(forward_length)):
+        x = self.train_data[:self.time_step]
+        y_seq = []
+        for i in tqdm(range(len(self.train_data) - self.time_step + future_step)):
             x, criteria, max_val = self.transform(x)
             y = self.graph_forward(self.model, np.asarray(x).reshape((1, self.time_step, 1)), False)
             x = np.append(x[1:], np.asarray(y).reshape(-1)[-1])
             x = self.inverse_transform(x, criteria, max_val)
             y_seq.append(float(x[-1]))
-
-        y_true = None
-        y_pred = None
-        if mode == 'train':
-            y_true = np.asarray(self.train_data[self.time_step:])
-            y_pred = np.asarray(y_seq)
-        else:
-            y_true = np.asarray(self.validation_data)
-            y_pred = np.asarray(y_seq)
-
-        loss = -1.0
+            if self.time_step + i < len(self.train_data):
+                x = np.append(x[:-1], self.train_data[self.time_step + i])
+        y_true = np.asarray(self.train_data[self.time_step:])
+        y_pred = np.asarray(y_seq)
+        mae = -1.0
         if future_step == 0:
-            loss = np.mean(np.abs(np.asarray(y_true - y_pred)))
-            print(f'validation loss : {loss:4f}\n')
+            mae = np.mean(np.abs(np.asarray(y_true - y_pred)))
+            print(f'train data MAE : {mae:4f}\n')
         if show_plot:
-            self.plot(y_true, y_pred)
-        return loss
+            self.plot([y_true, y_pred], [f'{self.ticker}', 'AI predicted'])
+        return mae
+
+    def evaluate_validation(self, show_plot=False, future_step=0):
+        print()
+        x = list(self.train_data[-self.time_step:])
+        y_seq = []
+        forward_length = len(self.validation_data) + future_step
+        for i in tqdm(range(forward_length)):
+            x, criteria, max_val = self.transform(x)
+            y = self.graph_forward(self.model, np.asarray(x).reshape((1, self.time_step, 1)), False)
+            x = np.append(x[1:], np.asarray(y).reshape(-1)[-1])
+            x = self.inverse_transform(x, criteria, max_val)
+            y_seq.append(float(x[-1]))
+            if self.time_step + i < len(self.validation_data):
+                x = np.append(x[:-1], self.validation_data[self.time_step + i])
+
+        y_true = np.asarray(self.validation_data)
+        y_pred = np.asarray(y_seq)
+        mae = np.mean(np.abs(np.asarray(y_true - y_pred[:len(y_true)])))
+        print(f'validation MAE : {mae:4f}\n')
+        if show_plot:
+            if future_step > 0:
+                y_true = y_true.tolist()
+                y_pred = y_pred.tolist()
+                y_pred_validation_only = y_pred[:len(y_true)]
+                y_pred_predicted_future = [None for _ in range(len(y_true) - 1)] + [y_pred_validation_only[-1]] + y_pred[len(y_true):]
+                self.plot([y_true, y_pred_validation_only, y_pred_predicted_future], [f'{self.ticker}', 'AI predicted', 'AI predicted future'])
+            else:
+                self.plot([y_true, y_pred], [f'{self.ticker}', 'AI predicted'])
+        return mae
