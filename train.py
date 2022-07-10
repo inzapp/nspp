699dd0e7ebb6f9405669e06501cb4b73ce5723b6
inzapp <inzapp@naver.com>
2022-07-10 18:45:59 +0900

    change default ticker name to build in sample_data.txt

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from nspp import NasdaqStockPricePredictor as NSPP
 
 if __name__ == '__main__':
     NSPP(
-        ticker='sample_data',
+        ticker='sample_data.txt',
         start_date='2020-01-01',
         end_date='2022-01-01',
         interval='1d',  # available interval : '1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'
